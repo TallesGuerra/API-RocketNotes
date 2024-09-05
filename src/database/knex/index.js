53b381d2f6a86cfc0daf0acb89c49ace7547a904
talles <talles-guerra@hotmail.com>
2024-09-05 14:11:53 +0100

    first commit

---
+++ src/database/knex/index.js
@@ -0,0 +1,9 @@
+/* https://knexjs.org/guide/ */
+
+const config = require("../../../knexfile");
+const knex = require("knex");
+
+
+const connection = knex(config.development);
+
+module.exports = connection;
