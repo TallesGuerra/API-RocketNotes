acf9df32141267548495bd9edf1be84a6e566b6e
talles <talles-guerra@hotmail.com>
2024-09-10 14:06:41 +0100

    uploads folder created; insomnia upload config

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,13 +1,25 @@
 const { Router } = require("express");
+const  multer = require("multer");
+const uploadConfig = require("../configs/upload")
 
 const UsersController = require("../controllers/usersController");
 const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
 
 const usersRoutes = Router();
+const upload = multer(uploadConfig.MULTER);
 
 const usersController = new UsersController();
 
+/* 
+    put » quando queremos atualizar propriedades dentro do banco de dados
+    patch » quando queremos atualizar um campo específico dentro do banco de dados.
+*/
+
 usersRoutes.post("/", usersController.create);
 usersRoutes.put("/", ensureAuthenticated, usersController.update);
+usersRoutes.patch("/avatar", ensureAuthenticated, upload.single("avatar"), (request, response) => {
+    console.log(request.file.filename);
+    response.json();
+})
 
 module.exports = usersRoutes;
